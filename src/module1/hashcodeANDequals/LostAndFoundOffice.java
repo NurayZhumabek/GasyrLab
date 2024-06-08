36a89795005b3b251900ca1c62c661bebf30feda
NurayZhumabek <nurazh3004@gmail.com>
2024-06-08 11:59:22 +0500

    update

--- src/module1/hashcodeANDequals/LostAndFoundOffice.java
+++ src/module1/hashcodeANDequals/LostAndFoundOffice.java
@@ -6,30 +6,68 @@ import java.util.List;
 public class LostAndFoundOffice {
     List<Object> things =new ArrayList<>();
 
-    boolean  check(Object o) {
+    boolean check(Object o) {
         if (o == null) return false;
-        for (Object object : things) {// логика проверки вещи в списке
+        for (Object object : things) {
             if (object.equals(o))
-                return  true;
-
-
+                return true;
         }
         return false;
-
     }
-
     void put(Object o){
         if (o!=null){
-            for (Object object:things) {
-                     things.add(o);
-                }
+            things.add(o);}
 
-            }
+
+
         }
     }
 
 
-class Ball{}
-class MusicalInstruments{}
-class Hat{}
-class Umbrella{}
+class Ball {
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Ball;
+    }
+
+    @Override
+    public int hashCode() {
+        return 1; // or any constant value
+    }
+}
+
+class MusicalInstruments {
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof MusicalInstruments;
+    }
+
+    @Override
+    public int hashCode() {
+        return 2; // or any constant value
+    }
+}
+
+class Hat {
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Hat;
+    }
+
+    @Override
+    public int hashCode() {
+        return 3; // or any constant value
+    }
+}
+
+class Umbrella {
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Umbrella;
+    }
+
+    @Override
+    public int hashCode() {
+        return 4; // or any constant value
+    }
+}
