f46bc1465db8ad736fb969f556f661706b138e63
NurayZhumabek <nurazh3004@gmail.com>
2024-06-08 13:17:46 +0500

    update

--- src/module1/exceptions/glab/Main.java
+++ src/module1/exceptions/glab/Main.java
@@ -1,16 +1,21 @@
 package module1.exceptions.glab;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
      try {
-         requestAge(160);
+         int age=sc.nextInt();
+         requestAge(age);
+         System.out.format("Input answer: %d", age);
      } catch (InvalidAgeException e){
          e.printStackTrace();
      }
     }
 
    static void requestAge(int age) throws InvalidAgeException{
-        if (age<1 && age>150){
+        if (age<1 || age>150){
             throw new InvalidAgeException("The input value for this range is not valid!");
         }
    }
